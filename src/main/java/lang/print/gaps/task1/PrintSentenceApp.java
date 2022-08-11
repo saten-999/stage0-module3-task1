e10749dcf9d3af87b3e25758ffcfac8a0d79f753
Abrahamyan <asatenik@mgc.mentorg.com>
2022-08-11 11:03:49 +0400

    done

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -1,9 +1,8 @@
 package lang.print.gaps.task1;
-import java.util.logging.Logger;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger( PrintSentenceApp.class.getName());
-        logger.info("This is my first Java program\n");
+
+        System.out.println("This is my first Java program\n");
     }
 }
--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task1;
 
 public class UpdatedApp {
-    public static void main() {
-        System.err.println("I'm updated\ncan be compiled. And can write to console\n");
+    public static void main(String[] args) {
+        System.out.println("I'm updated\ncan be compiled. And can write to console\n");
     }
 }
